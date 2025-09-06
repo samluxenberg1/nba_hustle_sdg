3577e98c7fb3a8e32bf9a4ee476020b00493c584
samluxenberg1 <samluxenberg1@gmail.com>
2025-09-05 22:08:14 -0400

    reorganizing

--- src/steps/ingest/process_data.py
+++ src/steps/ingest/process_data.py
@@ -68,6 +68,7 @@ class ProcessData:
 
         return self.df_proc_hustle
     
+    
     def process_data(self, output_dir: str):
         
         # Step 1 - Process Game Logs
@@ -76,13 +77,17 @@ class ProcessData:
         # Step 2 - Process Hustle Data
         df_proc_hustle = self.process_hustle()
 
-        # Step 3 - Join Game Logs and Hustle
+        # Step 3 - Process Four Factors Data
+        #df_proc_four_factors = self.process_four_factors()
+
+        # Step 4 - Join Game Logs, Hustle, Four Factors
         df_proc_hustle.drop(['HOME_TEAM_ID','AWAY_TEAM_ID'], axis=1, inplace=True)
         self.df_proc = self.df_proc_logs.merge(
             self.df_proc_hustle, 
             on='GAME_ID',
             how='inner'
         )
+
 
         # Step 4 - Save data
         os.makedirs(output_dir, exist_ok=True)
